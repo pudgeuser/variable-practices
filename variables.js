f3b1a4bc9098b5fd68820c7911cf200d3e8b033e
John Bryan Lomuntad <pudgeuser259@gmail.com>
2024-09-11 16:41:21 +0800

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'Lomuntad, John Bryan D.';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store 'Tattoo Artist'
+let whatDoYouWannaBecomeInYourLife = 'Tattoo Artist'
+
+// Create a variable named gender and store your gender
+let gender = 'Male';
+
+// Create a variable named FaceBookHandle and store your FaceBook Handle
+let FaceBookHandle = 'John Bryan';
+
+// Log all the variables to the console
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ', whatDoYouWannaBecomeInYourLife);
+console.log('Are you a Male or Female? ',gender);
+console.log('My FaceBook: ', FaceBookHandle);
